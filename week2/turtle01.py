1adfbaf4467ba47e54ed04d0846f3434bae0eb6d
dttung79 <dttung@gmail.com>
2026-09-25 11:55:34 +0700

    complete w2

---
+++ week2/turtle01.py
@@ -0,0 +1,14 @@
+import turtle as t
+
+side = 200
+angle = 90
+# draw a square
+t.forward(side)
+t.left(angle)
+t.forward(side)
+t.left(angle)
+t.forward(side)
+t.left(angle)
+t.forward(side)
+
+t.exitonclick()
